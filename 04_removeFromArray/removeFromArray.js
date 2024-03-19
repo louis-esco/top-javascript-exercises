a7f321ea90ec0124e3f06df4e6dfde7bc3126706
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-03-19 18:14:32 +0100

    Removes argument from input array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...remove) {
+    let filteredArray = array;
+    for (const toRemove of remove) {
+        filteredArray = filteredArray.filter(function (item) {
+            return item !== toRemove;
+        })
+    }
+    return filteredArray;
 };
 
 // Do not edit below this line
