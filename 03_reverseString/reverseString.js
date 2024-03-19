120716f09d907e84e59eaf814913fc005d3c5bf7
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-03-19 17:30:18 +0100

    Reverses the input string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    const stringArray = string.split("");
+    const reverseArray = stringArray.reverse();
+    const joinedArray = reverseArray.join("");
+    return joinedArray;
 };
 
 // Do not edit below this line
