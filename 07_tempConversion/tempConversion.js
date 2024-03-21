9c65c70555fc4d2f8b9ca421c2bb255c973fc77e
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-03-21 09:13:55 +0100

    Converts temps and round to 1 decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degF) {
+  let degC = Math.round(((degF - 32) * (5 / 9)) * 10) / 10;
+  return degC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degC) {
+  let degF = Math.round((degC * 9 / 5 + 32) * 10) / 10;
+  return degF;
 };
 
 // Do not edit below this line
