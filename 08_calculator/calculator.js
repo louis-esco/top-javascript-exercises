6a778433f594b6801c7b0d7cff483fdc9647cc74
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-03-24 15:25:25 +0100

    Creates calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,24 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((accumulator, value) => accumulator + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((accumulator, value) => accumulator * value, 1);
+};
+const power = function (a, b) {
+  return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (a) {
+  return a === 0 ? 1 : a * factorial(a - 1);
 };
 
 // Do not edit below this line
