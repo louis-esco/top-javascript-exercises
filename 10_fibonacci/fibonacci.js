f5345686d851da8afeeac6422b9a1c31a63e4071
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-03-24 15:54:10 +0100

    Calculates fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+    let int = parseInt(a);
+    if (int < 0) {
+        return 'OOPS';
+    } else if (int === 0) {
+        return 0;
+    } else if (int === 1) {
+        return 1;
+    } else {
+        return (fibonacci(int - 2) + fibonacci(int - 1));
+    }
 };
 
 // Do not edit below this line
