52eecd6185a55d428cb48b69a05e1874930077bd
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-03-24 15:36:09 +0100

    Detects palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let replaced = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s/g, "");
+    let reversed = replaced.split("").reverse().join("");
+    return replaced.toUpperCase() === reversed.toUpperCase();
 };
 
 // Do not edit below this line
